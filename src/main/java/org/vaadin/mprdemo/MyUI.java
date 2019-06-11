7ede297dd86b67f52b33fd2a5acef913d308c31c
Olli Tietäväinen <ollit@vaadin.com>
2019-06-11 14:54:00 +0300

    demo with mpr-v7 and Spreadsheet

--- src/main/java/org/vaadin/mprdemo/MyUI.java
+++ src/main/java/org/vaadin/mprdemo/MyUI.java
@@ -1,7 +1,9 @@
 package org.vaadin.mprdemo;
 
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.Version;
 import com.vaadin.mpr.core.HasLegacyComponents;
 import com.vaadin.mpr.core.MprTheme;
 import com.vaadin.ui.TabSheet;
@@ -12,9 +14,9 @@ public class MyUI extends FlexLayout implements HasLegacyComponents {
 
     public MyUI () {
     	setSizeFull();
+    	add(new Span("This is a Vaadin 14 beta 2 (Flow " + Version.getFullVersion() + ") app with mpr-v7 and Spreadsheet"));
         final TabSheet layout = new TabSheet();
         layout.addTab(new SpreadsheetView(),"Spreadsheet");
-        layout.addTab(new VideoView(),"Video");
         
         add(layout);
     }
